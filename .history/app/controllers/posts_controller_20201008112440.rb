54a51407bbb9df7ff5823141659642d32448fa53
kobayashi-kenta <12321klj@gmail.com>
2020-10-21 18:24:08 +0900

    first commit

---
+++ .history/app/controllers/posts_controller_20201008112440.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+
+  def index
+    @post= "確認用"
+  end
+
+end
+
---
+++ .history/app/controllers/posts_controller_20201008192015.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+
+  def index
+    @post= Post.find(1)
+  end
+
+end
+
